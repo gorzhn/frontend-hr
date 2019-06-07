0045fe118173cef032d45556f60d45dadf01ea89
gorzhn <gmalevski@gmail.com>
2019-06-07 17:35:05 +0200

    Denied/Refused

--- src/components/pages/sections/Modal.js
+++ src/components/pages/sections/Modal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 
 
